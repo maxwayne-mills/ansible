42022a4500d4f0c1d49164bfee5f9bcc3e9ccfed
Clarence Mills <cmills@opensitesolutions.com>
2016-06-08 11:37:51 -0400

    Changed Name to Open Site Solutions and provided comment explain use of that field

--- roles/zabbix/files/zabbix.conf.php
+++ roles/zabbix/files/zabbix.conf.php
@@ -14,7 +14,8 @@ $DB['SCHEMA'] = '';
 
 $ZBX_SERVER      = 'localhost';
 $ZBX_SERVER_PORT = '10051';
-$ZBX_SERVER_NAME = 'zabbix';
+// Change Open Site Solutions to what ever you want, this is displayed top left of the zabbix web interface
+$ZBX_SERVER_NAME = 'Open Site Solutions';
 
 $IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG;
 ?>
