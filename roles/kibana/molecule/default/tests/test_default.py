3134e5350bac47a55b4a27c691db0c6c859680f3
Clarence Mills <clarence.mills@gmail.com>
2018-02-17 20:20:37 -0500

    merge update

--- roles/kibana/molecule/default/tests/test_default.py
+++ roles/kibana/molecule/default/tests/test_default.py
@@ -10,5 +10,5 @@ def test_hosts_file(host):
     f = host.file('/etc/hosts')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.user == 'cmills'
+    assert f.group == 'cmills'
